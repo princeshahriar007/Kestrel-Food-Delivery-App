71f7cdc3df2d82a92b4abec15a24a5b33248ca72
Prince Shahriar <princeshahriar2013@live.com>
2022-04-25 14:27:24 +0600

    App Icon Added in onboarding Screen

--- screens/OnBoarding/OnBoarding.js
+++ screens/OnBoarding/OnBoarding.js
@@ -1,21 +1,45 @@
-import React from 'react';
-import {
-    View,
-    Text
-} from 'react-native';
+import React from "react";
+import { View, Text, ImageBackground, Image, Animated } from "react-native";
+
+import { constants, images, FONTS, SIZES, COLORS } from "../../constants";
 
 const OnBoarding = () => {
+  function renderHeaderLogo() {
     return (
-        <View
-            style={{
-                flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
-            }}
-        >
-            <Text>OnBoarding</Text>
-        </View>
-    )
-}
+      <View
+        style={{
+          position: "absolute",
+          top: SIZES.height > 800 ? 50 : 25,
+          left: 0,
+          top: 0,
+          right: 0,
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: SIZES.padding,
+        }}
+      >
+        <Image
+          source={images.logo_01}
+          resizeMode="contain"
+          style={{
+            width: SIZES.width * 0.5,
+            height: 100,
+          }}
+        />
+      </View>
+    );
+  }
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: COLORS.white,
+      }}
+    >
+      {renderHeaderLogo()}
+    </View>
+  );
+};
 
 export default OnBoarding;
