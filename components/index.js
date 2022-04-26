59453fbd53409a090ce384fdbf299ea083fd76cc
Prince Shahriar <princeshahriar2013@live.com>
2022-04-26 15:18:33 +0600

    Social Login added in SignIn Screen/SignIn Screen Done

--- components/index.js
+++ components/index.js
@@ -1,5 +1,6 @@
 import TextButton from "./TextButton";
+import TextIconButton from "./TextIconButton";
 import FormInput from "./FormInput";
 import CustomSwitch from "./CustomSwitch";
 
-export { TextButton, FormInput, CustomSwitch };
+export { TextButton, TextIconButton, FormInput, CustomSwitch };
