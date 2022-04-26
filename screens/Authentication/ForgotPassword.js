37110de6974e6c415127e01b29f7236d3a14bd40
Prince Shahriar <princeshahriar2013@live.com>
2022-04-27 01:50:29 +0600

    ForgotPassword Screen done

--- screens/Authentication/ForgotPassword.js
+++ screens/Authentication/ForgotPassword.js
@@ -1,17 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TouchableOpacity, Image } from "react-native";
 
 import { AuthLayout } from "../";
 import { FONTS, SIZES, COLORS, icons } from "../../constants";
 
-import { FormInput, CustomSwitch, TextButton } from "../../components";
+import { FormInput, TextButton } from "../../components";
 
-const ForgotPassword = () => {
+import { utils } from "../../utils";
+
+const ForgotPassword = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  function isEnableSendEmail() {
+    return email != "" && emailError == "";
+  }
   return (
     <AuthLayout
       title="Password Recovery"
       subtitle="Please enter your email address to recover your password"
-    ></AuthLayout>
+      titleContainerStyle={{
+        marginTop: SIZES.padding * 2,
+      }}
+    >
+      {/* Form Input */}
+      <View
+        style={{
+          flex: 1,
+          marginTop: SIZES.padding * 2,
+        }}
+      >
+        {/* Form Inputs */}
+        <FormInput
+          label="Email"
+          keyboardType="email-address"
+          autoCompleteType="email"
+          onChange={(value) => {
+            utils.validateEmail(value, setEmailError);
+            setEmail(value);
+          }}
+          errorMsg={emailError}
+          appendComponent={
+            <View
+              style={{
+                justifyContent: "center",
+              }}
+            >
+              <Image
+                source={
+                  email == "" || (email != "" && emailError == "")
+                    ? icons.correct
+                    : icons.cancel
+                }
+                style={{
+                  height: 20,
+                  width: 20,
+                  tintColor:
+                    email == ""
+                      ? COLORS.gray
+                      : email != "" && emailError == ""
+                      ? COLORS.green
+                      : COLORS.red,
+                }}
+              />
+            </View>
+          }
+        />
+      </View>
+      {/* Button */}
+      <TextButton
+        label="Send Email"
+        disabled={isEnableSendEmail() ? false : true}
+        buttonContainerStyle={{
+          height: 55,
+          alignItems: "center",
+          marginTop: SIZES.padding,
+          borderRadius: SIZES.radius,
+          backgroundColor: isEnableSendEmail()
+            ? COLORS.primary
+            : COLORS.transparentPrimary,
+        }}
+        onPress={() => navigation.goBack()}
+      />
+    </AuthLayout>
   );
 };
 
