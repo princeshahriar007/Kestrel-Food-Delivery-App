858690dc88dbea2f389ed6c665418fc63df66013
Prince Shahriar <princeshahriar2013@live.com>
2022-04-27 00:40:52 +0600

    OTP Input added in OTP Screen

--- screens/Authentication/Otp.js
+++ screens/Authentication/Otp.js
@@ -1,17 +1,50 @@
 import React from "react";
-import { View, Text, TouchableOpacity, Image } from "react-native";
+import { View, Text } from "react-native";
+import OTPInputView from "@twotalltotems/react-native-otp-input";
 
 import { AuthLayout } from "../";
-import { FONTS, SIZES, COLORS, icons } from "../../constants";
+import { FONTS, SIZES, COLORS } from "../../constants";
 
-import { FormInput, CustomSwitch, TextButton } from "../../components";
+import { TextButton } from "../../components";
 
-const Otp = () => {
+const Otp = ({ navigation }) => {
   return (
     <AuthLayout
       title="OTP Authentication"
       subtitle="An authentication code has been sent to your email address"
-    ></AuthLayout>
+      titleContainerStyle={{
+        marginTop: SIZES.padding * 2,
+      }}
+    >
+      {/* OTP Input */}
+      <View
+        style={{
+          flex: 1,
+          marginTop: SIZES.padding * 2,
+        }}
+      >
+        <OTPInputView
+          pinCount={4}
+          style={{
+            width: "100%",
+            height: 50,
+          }}
+          codeInputFieldStyle={{
+            width: 65,
+            height: 65,
+            borderRadius: SIZES.radius,
+            backgroundColor: COLORS.lightGray2,
+            color: COLORS.black,
+            ...FONTS.h3,
+          }}
+          editable
+          onCodeFilled={(code) => {
+            console.log(code);
+          }}
+        />
+      </View>
+      {/* Footer */}
+    </AuthLayout>
   );
 };
 
