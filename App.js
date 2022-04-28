313c61f99c65b59859aa94f5eee818d85b0124ae
Prince Shahriar <princeshahriar2013@live.com>
2022-04-28 17:43:04 +0600

    Redux initialized and App wrapped by provider

--- App.js
+++ App.js
@@ -3,6 +3,12 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import SplashScreen from "react-native-splash-screen";
 
+//Redux
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import rootReducer from "./stores/rootReducer";
+
 import {
   OnBoarding,
   SignIn,
@@ -15,6 +21,8 @@ import CustomDrawer from "./Navigation/CustomDrawer";
 
 const Stack = createStackNavigator();
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 const App = () => {
   const [token, setToken] = useState("fsf");
 
@@ -23,26 +31,28 @@ const App = () => {
   }, []);
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-        initialRouteName={token == "" ? "OnBoarding" : "Home"}
-      >
-        <Stack.Screen name="OnBoarding" component={OnBoarding} />
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+          initialRouteName={token == "" ? "OnBoarding" : "Home"}
+        >
+          <Stack.Screen name="OnBoarding" component={OnBoarding} />
 
-        <Stack.Screen name="SignIn" component={SignIn} />
+          <Stack.Screen name="SignIn" component={SignIn} />
 
-        <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="SignUp" component={SignUp} />
 
-        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+          <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
 
-        <Stack.Screen name="Otp" component={Otp} />
+          <Stack.Screen name="Otp" component={Otp} />
 
-        <Stack.Screen name="Home" component={CustomDrawer} />
-      </Stack.Navigator>
-    </NavigationContainer>
+          <Stack.Screen name="Home" component={CustomDrawer} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
