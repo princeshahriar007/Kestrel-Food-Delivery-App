5ecc430e6bc6d1e462124dd619bc9cf85ddb116d
Prince Shahriar <princeshahriar2013@live.com>
2022-04-30 14:34:17 +0600

    Tags Section added in FilterModal in HomeScreen

--- screens/Main/Home/FilterModal.js
+++ screens/Main/Home/FilterModal.js
@@ -198,6 +198,42 @@ const FilterModal = ({ isVisible, onClose }) => {
     );
   }
 
+  function renderTags() {
+    return (
+      <Section title="Tags">
+        <View
+          style={{
+            flexDirection: "row",
+            flexWrap: "wrap",
+          }}
+        >
+          {constants.tags.map((item, index) => {
+            return (
+              <TextButton
+                key={`Tags-${index}`}
+                label={item.label}
+                labelStyle={{
+                  color: item.id == tags ? COLORS.white : COLORS.gray,
+                  ...FONTS.body3,
+                }}
+                buttonContainerStyle={{
+                  height: 50,
+                  margin: 5,
+                  paddingHorizontal: SIZES.padding,
+                  alignItems: "center",
+                  borderRadius: SIZES.base,
+                  backgroundColor:
+                    item.id == tags ? COLORS.primary : COLORS.lightGray2,
+                }}
+                onPress={() => setTags(item.id)}
+              />
+            );
+          })}
+        </View>
+      </Section>
+    );
+  }
+
   return (
     <Modal animationType="fade" transparent={true} visible={isVisible}>
       <View
@@ -278,6 +314,9 @@ const FilterModal = ({ isVisible, onClose }) => {
 
             {/* Ratings */}
             {renderRatings()}
+
+            {/* Tags */}
+            {renderTags()}
           </ScrollView>
         </Animated.View>
       </View>
